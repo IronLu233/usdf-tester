7d39b751368394329f8741f4304b14657dade779
IronLu233 <ironlu233.dev@outlook.com>
2024-11-13 20:02:42 +0800

    init commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,80 @@
-import Image from "next/image";
+'use client'
+import { useTonAddress, useTonConnectUI } from '@tonconnect/ui-react';
+import { useExecuteTransfer, useExecuteBurn } from './useExecuteStake';
+import { useJettonWalletData } from './useTonSDK';
+import { useState } from 'react';
+
+const USDT_MASTER_ADDRESS = 'EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs';
+const USDF_MASTER_ADDRESS = 'EQB32vHWubvEv71kP-jdjoPQBi2dsb9V85tFw_60QjKTq-J-';
+const STAKING_ADDRESS = 'EQAJX24C8N5UiPxedga1B-Chd_TxRE87wYG4lO9bf8gG9L8W'
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [tonConnectUI] = useTonConnectUI();
+  const address = useTonAddress();
+  const { mutateAsync: exchangeUSDF } = useExecuteTransfer(USDT_MASTER_ADDRESS, USDF_MASTER_ADDRESS);
+  const { mutateAsync: burnUSDF } = useExecuteBurn(USDF_MASTER_ADDRESS);
+  const { data: USDFData } = useJettonWalletData(USDF_MASTER_ADDRESS);
+  const { data: stUSDFData } = useJettonWalletData(STAKING_ADDRESS);
+  const { mutateAsync: stakeUSDF } = useExecuteTransfer(USDF_MASTER_ADDRESS, STAKING_ADDRESS);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const [amount, setAmount] = useState('');
+  const [stakeUSDFAmount, setStakeUSDFAmount] = useState('');
+
+  if (!address) {
+    return <button onClick={() => tonConnectUI.openModal()}>Connect</button>
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const amountInMicro = (parseFloat(amount) * 10 ** 6).toString();
+    exchangeUSDF({ amount: amountInMicro });
+  };
+
+  const handleBurn = () => {
+    const amountInMicro = (parseFloat(amount) * 10 ** 6).toString();
+    burnUSDF({ amount: amountInMicro });
+  };
+
+  return (
+    <div>
+      <div>{address}</div>
+      <ul>
+        <li>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Exchange Amount:
+              <input
+                type="number"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              />
+            </label>
+            <button type="submit">Execute</button>
+            <button type="button" onClick={handleBurn}>Burn</button>
+          </form>
+          current USDF amount: {Number(USDFData?.balance ?? 0) / 10 ** 6}
+        </li>
+        <li>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              const amountInMicro = (parseFloat(stakeUSDFAmount) * 10 ** 6).toString();
+              stakeUSDF({ amount: amountInMicro });
+            }}
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <label>
+              Stake Amount:
+              <input
+                type="number"
+                value={stakeUSDFAmount}
+                onChange={(e) => setStakeUSDFAmount(e.target.value)}
+              />
+            </label>
+            <button type="submit">Stake</button>
+          </form>
+          stUSDF amount: {Number(stUSDFData?.balance ?? 0) / 10 ** 6}
+        </li>
+      </ul>
     </div>
   );
 }
